bddef6ab30da9a71bce34a9c37e5d7af8059b0a0
Peter Janak <2010janak2010@gmail.com>
2016-04-26 01:14:23 +0100

    Inserted google map, installed bourbon and normalize

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+
+  var mapInit = function(){
+    var map;
+    var mapOptions = {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    }
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+
+  }//mapInit() ends
+
+  mapInit();
+});

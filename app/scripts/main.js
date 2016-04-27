f13f3ec6bb7e9c10dfd0c003f417b0b578ef1677
Peter Janak <2010janak2010@gmail.com>
2016-04-27 03:28:01 +0100

    set marker listener

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -34,32 +34,40 @@ var initMap = function(){
 
 //place object
 var Place = function(data){
-  this.lat = data.lat;
-  this.lng = data.lng;
-  this.name = data.name;
+  this.lat = ko.observable(data.lat);
+  this.lng = ko.observable(data.lng);
+  this.name = ko.observable(data.name);
 
   //creates marker for this place
   this.marker = new google.maps.Marker({
-    position: {lat: this.lat, lng: this.lng},
+    position:  {lat:this.lat(), lng:this.lng()},
     map: map,
     draggable: true,
-    title: this.name,
+    title: this.name(),
     animation: google.maps.Animation.DROP
   });
 
-  this.info = function(){
+  //listens for click on marker
+  google.maps.event.addListener(this.marker, 'click', function() {
+    map.setZoom(15);
+    map.setCenter(this.getPosition());
 
-  };
+    if (this.getAnimation() !== null) {
+      this.setAnimation(null);
+    } else {
+      this.setAnimation(google.maps.Animation.BOUNCE);
+    }
+
+  });
 
   //runs animation on click
-  this.markerClick = function() {
+  this.itemClick = function() {
     if (this.marker.getAnimation() !== null) {
       this.marker.setAnimation(null);
     } else {
       this.marker.setAnimation(google.maps.Animation.BOUNCE);
     }
   };
-
 
 };
 
@@ -68,15 +76,23 @@ var ViewModel = function(data){
   self.data = data;
   self.places = ko.observableArray();//holds all places
 
-  //listens for click then calls objects click function
-  //el = element clicked on
-  self.markerClick = function(el){
-    el.markerClick();
+  //listens for click then calls object`s click function
+  self.itemClick = function(el){
+    //el = element clicked on
+    el.itemClick();
+
+    // center the map when location on the list is clicked
+    map.setCenter({lat: el.lat(), lng: el.lng()});
+    map.setZoom(15);
   };
 
   //creates array with all places
   self.data.forEach(function(place){
-    self.places.push(new Place(place));
+    self.places().push(new Place(place));
   });
+
+
+
+
 
 };

408ebf052f9c34c702dc6b677282ade5c58dc4a6
Peter Janak <2010janak2010@gmail.com>
2016-04-30 01:05:55 +0100

    binding map with view model, created Place class

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -21,3 +21,60 @@ var locations = [
 ];
 
 var map;
+
+
+var Place = function(data){
+  var self = this;
+  self.lat = ko.observable(data.lat);
+  self.lng = ko.observable(data.lng);
+  self.name = ko.observable(data.name);
+  self.position = ko.computed(function(){
+    return {lat: self.lat(), lng: self.lng()};
+  });
+
+
+
+};
+
+
+var MyViewModel = function() {
+    var self = this;
+    self.places = ko.observable([]);
+
+    for(var i =0; i<locations.length; i++){
+      self.places().push(new Place(locations[i]));
+
+    }
+}
+
+
+ko.bindingHandlers.googlemap = {
+  init: function (element, valueAccessor) {
+    var
+      value = valueAccessor(),
+      mapOptions = {
+        zoom: 11,
+        center: new google.maps.LatLng(value.centerLat, value.centerLon),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+        },
+      map = new google.maps.Map(element, mapOptions);
+
+    for(var i=0; i<value.places().length; i++){
+      var latLng = new google.maps.LatLng(
+                      value.places()[i].lat(),
+                      value.places()[i].lng()
+                    );
+      var marker = new google.maps.Marker({
+        position: latLng,
+        map: map
+      });
+    }
+  },
+
+  update: function(){
+    console.log('got it');
+  }
+};
+
+
+ko.applyBindings(new MyViewModel());
